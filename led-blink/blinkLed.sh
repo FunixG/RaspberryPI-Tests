fd33bc0919ba82e4041e5e5ffab0f7637e805958
Antoine PRONNIER (Funix) <antoine.pronnier@gmail.com>
2022-06-23 01:02:25 +0200

    first script

---
+++ led-blink/blinkLed.sh
@@ -0,0 +1,36 @@
+#!/bin/sh -l
+
+GPIO_FOLDER=gpio
+PATH_GPIO=/sys/class/${GPIO_FOLDER}
+PORT_GPIO=24
+
+removeAccess() {
+  cd ${PATH_GPIO} || echo "Folder ${PATH_GPIO} not existant" && exit
+  echo ${PORT_GPIO} > "unexport"
+  exit
+}
+
+turnOn() {
+  cd ${PATH_GPIO}/${GPIO_FOLDER}${PORT_GPIO} || echo "Folder led ${PORT_GPIO} not existant" && exit
+  echo "1" > "value"
+}
+
+turnOff() {
+  cd ${PATH_GPIO}/${GPIO_FOLDER}${PORT_GPIO} || echo "Folder led ${PORT_GPIO} not existant" && exit
+  echo "0" > "value"
+}
+
+cd ${PATH_GPIO} || echo "Folder ${PATH_GPIO} not existant" && exit
+echo ${PORT_GPIO} > "export"
+
+cd gpio${PORT_GPIO} || removeAccess
+
+echo "out" > "direction"
+
+turnOn
+sleep 1
+turnOff
+sleep 1
+turnOn
+
+removeAccess
