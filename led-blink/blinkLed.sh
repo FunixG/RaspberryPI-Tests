2df5e4d33a8fc49110bd567d03b5cffa69c7e31e
Antoine PRONNIER (Funix) <antoine.pronnier@gmail.com>
2022-06-23 02:36:05 +0200

    fix exit

--- led-blink/blinkLed.sh
+++ led-blink/blinkLed.sh
@@ -5,22 +5,22 @@ PATH_GPIO=/sys/class/${GPIO_FOLDER}
 PORT_GPIO=24
 
 removeAccess() {
-  cd ${PATH_GPIO} || echo "Folder ${PATH_GPIO} non existant" && exit 1
+  cd ${PATH_GPIO} || echo "Folder ${PATH_GPIO} non existant"
   echo ${PORT_GPIO} > "unexport"
   exit
 }
 
 turnOn() {
-  cd ${PATH_GPIO}/${GPIO_FOLDER}${PORT_GPIO} || echo "Folder led ${PORT_GPIO} non existant" && exit 1
+  cd ${PATH_GPIO}/${GPIO_FOLDER}${PORT_GPIO} || echo "Folder led ${PORT_GPIO} non existant"
   echo "1" > "value"
 }
 
 turnOff() {
-  cd ${PATH_GPIO}/${GPIO_FOLDER}${PORT_GPIO} || echo "Folder led ${PORT_GPIO} non existant" && exit 1
+  cd ${PATH_GPIO}/${GPIO_FOLDER}${PORT_GPIO} || echo "Folder led ${PORT_GPIO} non existant"
   echo "0" > "value"
 }
 
-cd ${PATH_GPIO} || echo "Folder ${PATH_GPIO} not existant" && exit 1
+cd ${PATH_GPIO} || echo "Folder ${PATH_GPIO} not existant"
 echo ${PORT_GPIO} > "export"
 
 cd gpio${PORT_GPIO} || removeAccess
